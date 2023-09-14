bc560e9e2ebdd461ca16ec8e8259ea148299b03c
vzem12 <144677767+vzem12@users.noreply.github.com>
2023-09-14 16:38:37 +0500

    Update dataGen.py

--- python/dataGen.py
+++ python/dataGen.py
@@ -5,39 +5,6 @@ import datetime
 import random
 import json
 
-channels_groups = {
-  1: {
-    'name': 'Новости',
-    'ch_start': 1,
-    'ch_end': 5
-  },
-  2: {
-    'name': 'Спорт',
-    'ch_start': 5,
-    'ch_end': 10
-  },
-  3: {
-    'name': 'Кино',
-    'ch_start': 11,
-    'ch_end': 15
-  },
-  4: {
-    'name': 'Музыка',
-    'ch_start': 16,
-    'ch_end': 20
-  },
-  5: {
-    'name': 'Шоу',
-    'ch_start': 21,
-    'ch_end': 25
-  },
-  6: {
-    'name': 'Позновательные',
-    'ch_start': 26,
-    'ch_end: 30
-  }
-}
-
 f = '%d.%m.%Y %H:%M:%S'
 
 class UserInfo():
