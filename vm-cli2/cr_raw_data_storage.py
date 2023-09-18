35441fd81dd5f1c569683cfe24a306633ad14914
vzem12 <144677767+vzem12@users.noreply.github.com>
2023-09-18 12:26:20 +0500

    Update cr_raw_data_storage.py

--- vm-cli2/cr_raw_data_storage.py
+++ vm-cli2/cr_raw_data_storage.py
@@ -1,4 +1,5 @@
 from fastavro import writer
+from hdfs.client import Client
 
 schema = {
   'doc': 'zemtsov_raw_data',
@@ -7,11 +8,15 @@ schema = {
   'type': 'record',
   'fields': [
     {'name': 'id', 'type': 'long'},
-    {'name': 'date', 'type': 'long'},
+    {'name': 'date', 'type': 'long', 'logicalType': 'timestamp-millis'},
     {'name': 'channel_id', 'type': 'int'},
     {'name': 'action', 'type': 'boolean'},
   ],
 }
 
-with open('weather.avro', 'wb') as out:
+with open('zemtsov_raw_data.avro', 'wb') as out:
     writer(out, schema)
+
+client = Client("hdfs://vm-dlake2-m-1:8020/",root="/user/zemtsov",timeout=10000,session=False)
+client.makedirs('/user/zemtsov/data')
+client.upload('data/zemtsov_raw_data.avro', 'zemtsov_raw_data.avro', cleanup=True)
