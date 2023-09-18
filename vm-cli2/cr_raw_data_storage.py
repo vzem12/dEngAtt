2e947604b67193edf708439b4d9c3aa0762799e3
vzem12 <144677767+vzem12@users.noreply.github.com>
2023-09-18 13:24:17 +0500

    Update cr_raw_data_storage.py

--- vm-cli2/cr_raw_data_storage.py
+++ vm-cli2/cr_raw_data_storage.py
@@ -18,5 +18,7 @@ with open('zemtsov_raw_data.avro', 'wb') as out:
     writer(out, schema)
 
 client = Client("hdfs://vm-dlake2-m-1:8020/",root="/user/zemtsov",timeout=10000,session=False)
-client.makedirs('/user/zemtsov/data')
-client.upload('data/zemtsov_raw_data.avro', 'zemtsov_raw_data.avro', cleanup=True)
+if not client.exists('/user/zemtsov/data/zemtsov_raw_data.avro'):
+  if not client.exists('/user/zemtsov/data'):
+    client.makedirs('/user/zemtsov/data')
+  client.upload('data/zemtsov_raw_data.avro', 'zemtsov_raw_data.avro', cleanup=True)
