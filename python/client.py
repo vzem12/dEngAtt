79aa9dc5201402a3c5b01d80203143ea2b1ec11c
vzem12 <144677767+vzem12@users.noreply.github.com>
2023-09-18 23:58:54 +0500

    Update client.py

--- python/client.py
+++ python/client.py
@@ -1,12 +1,20 @@
 from kafka import KafkaConsumer
 from threading import Thread
-from server import number_of_users
 import pg8000
 import json
+from pyspark.sql import SparkSession, Row
+from datetime import datetime as dt
+import time
+
+f = '%Y-%m-%d %H:%M:%S'
+
+number_of_users = 999
 
 consumer = KafkaConsumer(bootstrap_servers='vm-strmng-s-1.test.local:9092',
                          group_id='my_group')
+
 topics = list()
+
 for id in range(number_of_users): 
   topics.append(f'iptv_user_{id+1}')
   
@@ -21,21 +29,57 @@ cursor = conn.cursor()
                                       
 raw_table = 'zemtsov_raw_data'
 
-cursor.execute(f'''CREATE WRITABLE EXTERNAL TABLE IF NOT EXISTS {raw_table} (
+cursor.execute(f'''CREATE TABLE IF NOT EXISTS {raw_table} (
                      id bigint,
                      date timestamp,
                      channel_id integer,
                      action boolean)
-                   LOCATION ('pxf://user/zemtsov/data/zemtsov_raw_data.avro?PROFILE=hdfs:AVRO&SERVER=Hadoop'
-                   FORMAT 'CUSTOM' (FORMATTER='pxfwritable_export')
+                   WITH (
+                     appendonly=true,
+                     compresstype=zlib,
+                     compresslevel=1,
+                     orientation=column)
+                   DISTRIBUTED BY (date);
                 ''') 
 cursor.execute(f'TRUNCATE TABLE {raw_table}')
 conn.commit()
+
+def toStorage(q):
+  spark = SparkSession.builder.getOrCreate()
+  lstTs = time.time()
+  rows = []
+  while clientRunning:
+    msg = q.get()
+    rows.append(msg['id'],
+                dt.strptime(msg['date'],f),
+                msg['channel_id'],
+                msg['action'])
+    if (time.time() - lstTs) >= interval and len(rows) != 0:
+      df = spark.createDataFrame(rows,schema='id long, date timestamp, channel_id int, action boolean')
+      df.write.mode('append').parquet(f'hdfs://vm-dlake2-m-1.test.local/user/zemtsov/data/{raw_table}.parquet')
+      rows=[]
+    lstTs = time.time()
+  spark.stop()
+
+def toGP(q):
+  while clientRunning:
+    msg = q.get()
+    cursor.execute(f'''INSERT INTO {raw_table} (id,date,channel_id,action) values({msg["id"]}, '{msg["date"]}'$
+    conn.commit()
+  
 try:
+  qStorage = Queue()
+  qGP = Queue()
+  storage_thread = Thread(target=toStorage,args=(qStorage,))
+  gp_thread = Thread(target=toGP,args=(qGP,))
+  storage_thread.start()
+  gp_thread.start()
+  
   for message in consumer:
     msg = json.loads(message.value.decode('cp1251'))
     #print(f"{msg['id']}, {msg['date']}, {msg['channel_id']}, {msg['action']}")
-    cursor.execute(f'''INSERT INTO {raw_table} (id,date,channel_id,action) values({msg["id"]}, '{msg["date"]}', {msg["channel_id"]}, {msg["action"]});''')
-    conn.commit()
+    qStorage.put(msg)
+    qGP.put(msg)
+
 finally:
   conn.close()
